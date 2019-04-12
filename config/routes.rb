821313d30233a673d5123431d1f78c6874d06635
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2019-04-12 14:10:44 -0300

    devise-token-auth configured

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
